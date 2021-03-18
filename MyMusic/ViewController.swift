4575ee0e4753c48fbbec07760b3222160ad01688
bintangaria <bintangstarfreak27@gmail.com>
2021-03-18 09:15:59 +0700

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Bintang Aria Ramadhan on 18/03/21.
+//  Copyright © 2021 Starfreak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
